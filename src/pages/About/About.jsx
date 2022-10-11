65054f352acfa583319ddca33fe388567a8a9502
isa tepsurkaev <isatepsurkaev@MacBook-Pro-isa.local>
2022-10-11 12:04:57 +0300

    qqq

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Footer/Footer";
 import styles from "./about.module.css";
 
 const About = React.memo(() => {
