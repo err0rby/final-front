9dc80361ac3b5cc88ff6f4139e4c12e0529f3ebf
isa tepsurkaev <isatepsurkaev@MacBook-Pro-isa.local>
2022-10-11 11:28:45 +0300

    about-ad

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import MainPage from "./pages/MainPage/MainPage";
 import OneAlphabetPage from "./pages/OneAlphabetPage/OneAlphabetPage";
 import OneAuction from "./pages/OneAuction/OneAuction";
 import Products from "./pages/Products/Products";
-
 import Alphabet from "./pages/Alphabet/Alphabet";
 import UserProfile from "./pages/userProfile/userProfile";
-
 
 
 function App() {
