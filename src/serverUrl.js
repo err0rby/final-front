94f2954016e9a87e14407ab26903010f41ea52d7
errorby <mdidayev@inbox.ru>
2022-10-19 18:22:56 +0300

    deploy3

--- src/serverUrl.js
+++ src/serverUrl.js
@@ -5,5 +5,5 @@ export let serverUrl;
 if (NODE_ENV === 'development') {
     serverUrl = 'http://localhost:3030' // адрес сервера на локалке
 } else {
-    serverUrl = 'https://auction.onrender.com'; // адрес сервера после выгрузки
+    serverUrl = 'https://auction-backend.onrender.com'; // адрес сервера после выгрузки
 }
